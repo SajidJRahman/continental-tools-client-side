5c11799a6207f121a678d70b4b2b1319abc4bc48
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-22 21:28:00 +0200

    added business summary

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Footer from '../../Shared/Footer/Footer';
-import './Home.css';
 import Newsletter from '../Newsletter/Newsletter';
+import BusinessSummery from '../BusinessSummery/BusinessSummery';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
             <Banner />
-            <Newsletter/>
+            <BusinessSummery />
+            <Newsletter />
             <Footer />
         </div>
     );
