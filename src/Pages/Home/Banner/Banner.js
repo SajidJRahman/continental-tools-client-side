e12fb86db379f70d1d9a844ad867c7128a0f537d
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-22 16:10:04 +0200

    added footer

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './Banner.css';
+import Tools from '../../../Images/Tools/tools.jpg';
 
 const Banner = () => {
     return (
-        <div>
-
-        </div>
+        <div className="hero min-h-screen" style={{
+            backgroundImage: `url(${Tools})`
+        }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Welcome To Continental Tools</h1>
+                    <p className="mb-5">The home for all the tools you need. Need decent amount of tools? You're at the right place!</p>
+                    <button className="btn btn-primary px-8">View Items</button>
+                </div>
+            </div>
+        </div >
     );
 };
 
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Footer from '../../Shared/Footer/Footer';
 import './Home.css';
 
 const Home = () => {
     return (
         <div>
-
+            <Banner />
+            <Footer />
         </div>
     );
 };
