e34785c7a4cd05ef9aae57eed71021d1da013472
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-27 22:53:09 +0200

    modified README.md

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -221,9 +221,9 @@ const MyPortfolio = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
                             <p className='font-bold'>GitHub</p>
-                            <p className='font-semibold'>/sajidjrahman</p>
+                            <p className='font-semibold'>github.com/sajidjrahman</p>
                             <p className='flex justify-center items-center text-info'>
-                                <a className='mr-1 font-semibold' rel="noreferrer" target='_blank' href="https://facebook.com/sajidjrahman" >GitHub</a>
+                                <a className='mr-1 font-semibold' rel="noreferrer" target='_blank' href="https://github.com/sajidjrahman" >GitHub</a>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-info" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
                                 </svg>
