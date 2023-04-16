ee3da80b8a5da707f2c650f87fdfda8256986f8c
SajidJRahman <sajidjrahman@icloud.com>
2023-04-16 22:17:14 +0200

    fixed some issues

--- continental-tools-client/src/Pages/Home/Banner/Banner.js
+++ continental-tools-client/src/Pages/Home/Banner/Banner.js
@@ -9,6 +9,7 @@ const Banner = () => {
         }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
+                {/* <div>this is just a comment`</div> */}
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold">Welcome To Continental Tools</h1>
                     <p className="mb-5">The home for all the tools you need. Need decent amount of tools? You're at the right place!</p>
